135a8fb51e07c7cb9be5e188a8dcbebd76d68ae3
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-03-12 09:26:58 -0300

    introducao colletions

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    val nomes: Collection<String> = listOf(
+        "walter",
+        "heitor",
+        "freitas",
+        "oliveira",
+        "maria",
+        "clara"
+    )
+    for(nome: String in nomes){
+        println(nome)
+        //println(nome.iterator())
+    }
+    println(nomes)
+    println("Tem o nome walter?  ${nomes.contains("walter")}")
+    println("Tamanho da colecao ${nomes.size}")
+}
