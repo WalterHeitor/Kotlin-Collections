a7c510c12f4b457202f0fbf67179eda28a7f1081
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-03-13 12:15:55 -0300

    funcoes lambidas com retorno

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,19 +2,68 @@ import banco.pedido.Pedido
 
 fun main() {
 
-    val minhaFuncao : ()-> Unit = ::teste
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+    testeFuncaoLambida()
+    testeFuncaoLambidaRetorno()
+    println(minhaFuncaoAnonima)
+    testaTipoFuncaoClasseSoma()
+    somaReferencia()
+}
 
-    println(soma(43, 61))
-    println(minhaFuncao())
+fun testeFuncaoLambida() {
+    val minhaFuncaoLambida: () -> Unit = {
+        println("executa como lambida")
+    }
+    println(minhaFuncaoLambida)
+    println(minhaFuncaoLambida())
+}
 
+fun testeFuncaoLambidaRetorno() {
+    val minhaFuncaoLambida: (Int, Int, Int) -> Int = { a, b, _ ->
+        println("funcao Labida que retorna")
+        a + b
+    }
+    println(minhaFuncaoLambida)
+    println(minhaFuncaoLambida(20, 40, 1))
+    println(minhaFuncaoAnonimaRetorno(49,20,10))
+}
+
+val minhaFuncaoAnonima: () -> Unit =fun (){  // funcao anonima
+    println("executa como anonima")
+}
+val minhaFuncaoAnonimaRetorno: (Int, Int, Int) -> Int =fun (a: Int, b: Int, _: Int): Int{  // funcao anonima
+    println("executa como anonima Retorno")
+    return a + b
+}
+
+
+fun testaTipoFuncaoClasse() {
     val minhaFuncaoClasses: () -> Unit = Teste()
     println(minhaFuncaoClasses) //instancia de teste
     println(minhaFuncaoClasses()) // () executar
 }
 
+fun testaTipoFuncaoClasseSoma() {
+    val minhaFuncaoClassesSoma: (Int, Int) -> Int = Soma()
+    println(minhaFuncaoClassesSoma) //instancia de teste
+    println(minhaFuncaoClassesSoma(5,10)) // () executar
+}
+
+fun testaTipoFuncaoReferencia() {
+    val minhaFuncao: () -> Unit = ::teste //referencia de uma fucao
+
+    println(soma(43, 61))
+    println(minhaFuncao())
+}
+
 fun soma(a: Int, b: Int) : Int = a + b
 fun teste(){
     println("executa teste")
+}
+fun somaReferencia(){
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5,10))
 }
 
 class Teste : () -> Unit{
@@ -23,7 +72,12 @@ class Teste : () -> Unit{
     }
 
 }
+class Soma : (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int {
+       return  p1 + p2
+    }
 
+}
 
 
 
